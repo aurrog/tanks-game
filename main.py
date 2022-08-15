f371b4f3574835584e555fbb5c2eaa8adeab23f4
aurrog <gnusovme@gmail.com>
2022-08-15 14:35:25 +0300

    update all

--- main.py
+++ main.py
@@ -1,58 +1,26 @@
-import sys
-
 import pygame
+from settings import *
+from player import Player
+from map import world_map
 
 
-WIDTH = 500
-HEIGHT = 500
-FPS = 60
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-
-tank_surf = pygame.image.load('images/tank.png').convert()
-tank_rect = tank_surf.get_rect(center=(WIDTH//2, HEIGHT//2))
-
-tank_up = tank_surf
-tank_down = pygame.transform.flip(tank_surf, 0, 1)
-tank_left = pygame.transform.rotate(tank_surf, 90)
-tank_right = pygame.transform.rotate(tank_surf, -90)
-
-tank_surf = pygame.transform.scale(tank_surf, (tank_surf.get_width()*2, tank_surf.get_height()*2))
-
-tank = tank_up
-speed = 5
+player = Player()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            sys.exit()
+            exit()
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        tank = tank_left
-        tank_rect.x -= speed
-        if tank_rect.x < 0:
-            tank_rect.x = 0
-    elif keys[pygame.K_RIGHT]:
-        tank = tank_right
-        tank_rect.x += speed
-        if tank_rect.x > WIDTH-tank_rect.height:
-            tank_rect.x = WIDTH-tank_rect.height
-    elif keys[pygame.K_UP]:
-        tank = tank_up
-        tank_rect.y -= speed
-        if tank_rect.y < 0:
-            tank_rect.y = 0
-    elif keys[pygame.K_DOWN]:
-        tank = tank_down
-        tank_rect.y += speed
-        if tank_rect.y > HEIGHT-tank_rect.height:
-            tank_rect.y = HEIGHT-tank_rect.height
+    player.movement()
+    sc.fill(BLACK)
 
-    screen.fill((0, 0, 0))
-    screen.blit(tank, tank_rect)
+    pygame.draw.circle(sc, GREEN, player.pos, 12)
 
-    pygame.display.update()
+    for x, y in world_map:
+        pygame.draw.rect(sc, DARKGRAY, (x, y, TILE, TILE))
 
+    pygame.display.flip()
     clock.tick(FPS)
---
+++ settings.py
@@ -0,0 +1,20 @@
+WIDTH = 780
+HEIGHT = 780
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 60
+TILE = 60
+
+# player settings
+player_pos = (5*TILE-TILE/2, 13*TILE-TILE/2)
+player_angle = 0
+player_speed = 2
+
+# colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (220, 0, 0)
+GREEN = (0, 220, 0)
+BLUE = (0, 0, 255)
+DARKGRAY = (40, 40, 40)
+PURPLE = (120, 0, 120)
